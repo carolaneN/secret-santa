77952b1700447352664a8c1d9b5b9a382ae7c2bc
carolaneN <carolane.noel@etu.unilim.fr>
2024-12-19 08:34:41 +0100

    Updates

--- src/components/Button.jsx
+++
@@ -1,6 +0,0 @@
-
-export function Button ({ input, onClick }) {
-    return (
-        <button onClick={onClick} className="button text-lg px-8 py-3 text-white bg-pine-santa rounded-full cursor-pointer focus:bg-green-santa">{input}</button>
-    );
-}
